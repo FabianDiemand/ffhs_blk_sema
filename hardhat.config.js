4501fd22336c34e78539fe43bda5570df06304c0
Fabian Diemand <fabian.diemand@hotmail.com>
2023-12-20 19:30:14 +0100

    feature(verify): verify smart contract with etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('dotenv').config();
 require('@nomiclabs/hardhat-ethers');
+require('@nomicfoundation/hardhat-verify');
 
 const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.22",
@@ -14,4 +16,7 @@ module.exports = {
         accounts: [`0x${METAMASK_PRIVATE_KEY}`]
     }
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
